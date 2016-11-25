6057ea8b805b146f99c3cf1f986126f8e787438f
Roger Sherman <rsherman@viddler.com>
2016-11-25 22:15:41 +1100

    Basically pseudocode at this point.

---
+++ work.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	temp := // current temperature
+	dewp := // current dew point
+	
+	if temp + dewp >= 150 {
+		fmt.Println("Tejas Richard says do it indoors.")
+	} else {
+		fmt.Println("Tejas Richard says go do it outdoors!")
+	}
+}
