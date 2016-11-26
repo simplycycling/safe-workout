12dbf5e2872bd564b4e15345e4ce0bd22d34a45e
Roger Sherman <rsherman@viddler.com>
2016-11-26 16:55:34 +1100

    This is never going to work.

--- work.go
+++ work.go
@@ -4,9 +4,9 @@ import "fmt"
 
 func main() {
 	var temp int
-	_, err := fmt.Scanf("%d", &temp)
+	_, err := fmt.Scanf("What is the current temperature? %d", &temp)
 	var	dewp int
-	_, err := fmt.Scanf("%d", &dewp)
+	_, err := fmt.Scanf("And what is the current dew point? %d", &dewp)
 	
 	if temp + dewp >= 150 {
 		fmt.Println("Tejas Richard says do it indoors.")
